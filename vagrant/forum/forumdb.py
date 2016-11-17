bf3c125e66908e1762f9f2c25afc8c51e5a372a5
CUguru <uguru@sheridanc.on.ca>
2016-11-17 17:40:07 -0500

    Update forumdb.py

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -3,12 +3,16 @@
 # 
 
 import time
+import psycopg2
 
 ## Database connection
 DB = []
 
 ## Get posts from database.
 def GetAllPosts():
+    db = psycopg2.connect("dbname=forum")
+    connection = db.cursor()
+    connection.execute("SELECT time, content FROM posts ORDER BY time DESC")
     '''Get all the posts from the database, sorted with the newest first.
 
     Returns:
@@ -16,16 +20,21 @@ def GetAllPosts():
       pointing to the post content, and 'time' key pointing to the time
       it was posted.
     '''
-    posts = [{'content': str(row[1]), 'time': str(row[0])} for row in DB]
-    posts.sort(key=lambda row: row['time'], reverse=True)
+    posts = ({'content': str(row[1]), 'time': str(row[0])}
+             for row in connection.fethcAll())
+    db.close()
     return posts
 
 ## Add a post to the database.
 def AddPost(content):
+    db = psycopg2.connect("dbname=forum")
+    connection = db.cursor()
+    connection.execute("INSERT INTO posts (content) VALUES ('%s')" % content)
+    
+    db.commit()
+    db.close()
     '''Add a new post to the database.
 
     Args:
       content: The text content of the new post.
     '''
-    t = time.strftime('%c', time.localtime())
-    DB.append((t, content))
